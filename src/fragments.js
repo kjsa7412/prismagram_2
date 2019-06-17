c23c9f8cded0809c5e4f96d77b162e9c440826c4
Kyungjin Sa <48097915+kjsa7412@users.noreply.github.com>
2019-06-17 17:51:46 +0900

    #3.25 seeRoom Resolver

--- src/fragments.js
+++ src/fragments.js
@@ -1,6 +1,7 @@
 export const USER_FRAGMENT = `
     id
     username
+    avatar
 `;
 
 export const COMMENT_FRAGMENT = `
@@ -10,10 +11,20 @@ export const COMMENT_FRAGMENT = `
         ${USER_FRAGMENT}
     }
 `;
-
 export const FILE_FRAGMENT = `
     id
     url
+`;
+
+export const MESSAGE_FRAGMENT = `
+    id
+    text
+    to {
+        ${USER_FRAGMENT}
+    }
+    from {
+        ${USER_FRAGMENT}
+    }
 `;
 
 export const FULL_POST_FRAGMENT = `
@@ -29,6 +40,17 @@ export const FULL_POST_FRAGMENT = `
         }
         user {
             ${USER_FRAGMENT}
+        }
+    }
+`;
+export const ROOM_FRAGMENT = `
+    fragment RoomParts on Room {
+        id
+        participants {
+            ${USER_FRAGMENT}
+        }
+        messages { 
+            ${MESSAGE_FRAGMENT}
         }
     }
 `;
